11e78bd45457500591fcb511be0392afd4d017a1
Dmitrii <38377296+DmRk-Vl@users.noreply.github.com>
2020-07-27 01:19:55 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+console.log("Тебе тут не рады");
+console.log("Съебал с консоли");
+
+function format(){
+  const inputText = document.getElementById("text").value;
+
+  if (!inputText.includes(".") && !inputText.includes(";")) {
+    document.getElementById("error").innerHTML = "Точку или точку с запятой введи, долбаеб"
+
+      return;
+  }
+
+  document.getElementById("container").classList.add("hidden");
+
+  setBg();
+  document.getElementById("result").innerHTML = inputText
+    .replace(/\./g, " я пукнул ")
+    .replace(/;/g, " я сука ");
+}
+
+function setBg(){
+  document.getElementById("body").classList.add("bg");
+}
+
+document.getElementById("btn").onclick = format;
+
+window.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        format();
+    }
+});
